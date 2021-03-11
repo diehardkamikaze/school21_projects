f63eea327871ab88a37bf3597c73103f0e105ce5
diehardkamikaze <diehardkamikaze@gmail.com>
2021-03-11 15:00:26 +0300

    start of new project

---
+++ ft_cub3D/includes/get_next_line.h
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mchau <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/16 08:37:44 by mchau             #+#    #+#             */
+/*   Updated: 2020/11/24 18:33:42 by mchau            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 32
+# endif
+
+int					get_next_line(int fd, char **line);
+
+char				*ft_substr(char *s, size_t start, size_t len);
+
+typedef struct		s_list
+{
+	char			*content;
+	struct s_list	*next;
+	int				len;
+}					t_list;
+
+typedef struct		s_o
+{
+	size_t	line_len;
+	t_list	*lst;
+	char	*buffer;
+	char	*divider;
+	long	i;
+	char	*tmp;
+}					t_o;
+
+t_list				*ft_lstnew(char *content);
+
+int					ft_lstadd_back(t_list **lst, t_list *new);
+
+int					ft_lstclear(t_list **lst);
+
+char				*ft_strchr(const char *s, int c);
+
+#endif
