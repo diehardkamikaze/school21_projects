02c182cff5c063d488d9af7b4aa0d752b72d5b6d
diehardkamikaze <diehardkamikaze@gmail.com>
2021-04-16 17:22:46 +0300

    tested sigaction

--- minishell/main.c
+++ minishell/main.c
@@ -6,18 +6,34 @@
 /*   By: mchau <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/16 13:52:52 by mchau             #+#    #+#             */
-/*   Updated: 2021/04/16 16:41:51 by mchau            ###   ########.fr       */
+/*   Updated: 2021/04/16 17:22:00 by mchau            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <signal.h>
 
-
-int main(void)
+void	before_exit(int sig)
 {
-	int pid;
+	printf("now i going to sleep");
+	exit(1);
+}
+
+int		main(void)
+{
+	int c;
+	struct sigaction action;
+
+	action.sa_handler = before_exit;
+	sigemptyset(&(action.sa_mask));
+	action.sa_flags = 0;
+	sigaction(SIGINT, &action, 0);
+	while((c=fgetc(stdin)) !=EOF) {
+		printf("%c", c);
+	}
+	/*	int pid;
 	int fd[2];
 
 	pipe(fd); //защити [ отсюда считываем, cюда записываем ]
@@ -33,5 +49,6 @@ int main(void)
 	dup2(fd[1], 1);
 	//while (1)
 		write(fd[1], "1", 1); //раньше пишем в буфер, чем запускается дочерний процесс, но разве это не кусок памяти?
+*/
 	return (0);
 }
