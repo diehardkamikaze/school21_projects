431cbdb610a31a4fbd8983f7fd5e5ed01fa5d709
diehardkamikaze <diehardkamikaze@gmail.com>
2021-04-21 19:08:37 +0300

    normed and changed flood_fill

--- ft_cub3D/cub_starter.c
+++ ft_cub3D/cub_starter.c
@@ -6,53 +6,11 @@
 /*   By: mchau <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/11 13:59:25 by mchau             #+#    #+#             */
-/*   Updated: 2021/03/24 13:54:53 by mchau            ###   ########.fr       */
+/*   Updated: 2021/04/21 10:49:41 by mchau            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_cub3D.h"
-
-void	free_wrapper(t_all *t)
-{
-	int i;
-
-	if (t)
-	{
-		if (t->plr)
-			free(t->plr);
-		if (t->maze)
-		{
-			i = 0;
-			while (i < 5)
-			{
-				if (t->maze->textures[i])
-					free(t->maze->textures[i]);
-				if (t->txt_img[i])
-					free(t->txt_img[i]);
-				i++;
-			}
-			free(t->maze);
-		}
-		if (t->spr)
-			free(t->spr);
-		if (t->map)
-		{
-			i = 0;
-			while (t->map[i])
-				free(t->map[i++]);
-		}
-		free(t);
-	}
-}
-
-void	exit_with_message(char *str, t_all *t)
-{
-	write(1, "Error\n", 6);
-	free_wrapper(t);
-	if (str)
-		ft_putstr(str);
-	exit(0);
-}
 
 int		main(int argc, char **argv)
 {
@@ -71,15 +29,11 @@ int		main(int argc, char **argv)
 	fd = open(argv[1], O_RDONLY);
 	if (fd == -1)
 		exit_with_message(strerror(errno), 0);
-	t = map_file_parser(fd); // забываешь всюду закрыть fd в случае парсинга;
-	close(fd);
+	t = map_file_parser(fd);
 	init_game(t);
-	if (argc >= 3 && ft_strncmp(argv[2], "--save", 6) == 0)
+	if (argc >= 3 && ft_strncmp(argv[2], "--save", 6) == 0 && argv[2][6] == 0)
 		bmp_maker(t);
 	else
 		go_game_logic(t);
-
-	free_wrapper(t);
-	//чисти все при --save и escape/[x]
 	return (0);
 }
--- ft_cub3D/flood_fill.c
+++ ft_cub3D/flood_fill.c
@@ -6,17 +6,30 @@
 /*   By: mchau <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/19 12:19:04 by mchau             #+#    #+#             */
-/*   Updated: 2021/03/24 14:12:31 by mchau            ###   ########.fr       */
+/*   Updated: 2021/04/21 10:21:21 by mchau            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_cub3D.h"
 
+void	sprite_handler(int coords, t_compose *t)
+{
+	t_spr_list *tmp;
+
+	if (!(tmp = malloc(sizeof(t_spr_list))))
+		map_error("MAP: Error on spr malloc", t, 0, -1);
+	tmp->coords = coords;
+	tmp->next = t->spr_struct;
+	t->spr_struct = tmp;
+}
+
 void	flood_fill(t_compose *t, int pos_x, int pos_y)
 {
-	if (0 > pos_x || pos_x > (t->max_x - 1) || pos_y > (t->max_y - 1) \
+	if (0 > pos_x || pos_x >= t->max_x || pos_y >= t->max_y \
 			|| 0 > pos_y || t->result->map[pos_x][pos_y] == ' ')
 		exit_with_message("FF: hole in wall!", t->result);
+	if (t->result->map[pos_x][pos_y] == '2')
+		sprite_handler(pos_x * 1000 + pos_y, t);
 	if (t->result->map[pos_x][pos_y] != '1' && \
 			t->result->map[pos_x][pos_y] != 'L')
 		t->result->map[pos_x][pos_y] = 'L';
