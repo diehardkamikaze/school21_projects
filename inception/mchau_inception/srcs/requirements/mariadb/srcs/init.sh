e74460fd0570eddd187d1a79f0e0ebace953e51f
Milissa Chau <mchau@ge-l6.kzn.21-school.ru>
2021-07-25 11:36:16 +0300

    fixed database root logging without password issue

--- inception/mchau_inception/srcs/requirements/mariadb/srcs/init.sh
+++ inception/mchau_inception/srcs/requirements/mariadb/srcs/init.sh
@@ -16,10 +16,15 @@
 service mysql start
 # Configure a wordpress database using pipe and mysql command
 echo "source /mchau.sql;" | mysql -u root --skip-password
-echo "CREATE USER 'root'@'%' IDENTIFIED BY 'root'; GRANT ALL ON *.* TO 'root'@'%' WITH GRANT OPTION; FLUSH PRIVILEGES;" | mysql -u root --skip-password
-echo "CREATE USER 'mchau'@'%' IDENTIFIED BY 'mchau'; GRANT ALL ON *.* TO 'root'@'%' WITH GRANT OPTION; FLUSH PRIVILEGES;" | mysql -u root --skip-password
+#echo "update user set password=PASSWORD("almaz") where User='root';" | mysql -u root --skip-password
+#echo "flush privileges;" | mysql -u root --skip-password
+#echo "fupdate user set plugin='' where User='root';" | mysql -u root --skip-password
+#echo "CREATE USER 'root'@'%' IDENTIFIED BY 'almaz'; GRANT ALL ON *.* TO 'root'@'%' WITH GRANT OPTION; FLUSH PRIVILEGES;" | mysql -u root --skip-password
+echo "CREATE USER 'mchau'@'%' IDENTIFIED BY 'mchau'; GRANT ALL ON *.* TO 'mchau'@'%' WITH GRANT OPTION; FLUSH PRIVILEGES;" | mysql -u root --skip-password
 echo "GRANT ALL PRIVILEGES ON mchau.* TO 'root'@'localhost' WITH GRANT OPTION;" | mysql -u root --skip-password
 echo "FLUSH PRIVILEGES;" | mysql -u root --skip-password
+echo "SET PASSWORD FOR root@'%'=PASSWORD('almaz');"| mysql -u root --skip-password
+echo "update mysql.user set authentication_string=PASSWORD('almaz'), plugin='mysql_native_password' where user='root';"| mysql -u root --skip-password
+mysql -e "alter user 'root'@'localhost' identified by 'almaz'";
 # echo "update mysql.user set plugin='' where user='root';" | mysql -u root --skip-password
 # nginx -g "daemon off;"
-
